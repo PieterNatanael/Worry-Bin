86724a39cb60b18ac1063d120ee59314aab16342
PieterNatanael <pieternatanael@icloud.com>
2024-04-16 21:12:15 +0800

    Initial Commit

---
+++ Worry Bin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Worry Bin
+//
+//  Created by Pieter Yoshua Natanael on 16/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Worry Bin/Worry_BinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Worry_BinApp.swift
+//  Worry Bin
+//
+//  Created by Pieter Yoshua Natanael on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Worry_BinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
